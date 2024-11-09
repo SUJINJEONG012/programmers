7e3aa259490f7f083d3245c0a9d1ffddf29897f1
Angela <56811978+SUJINJEONG012@users.noreply.github.com>
2024-11-09 22:41:06 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+# SELECT ai.ANIMAL_ID, ao.NAME
+# from ANIMAL_INS as ai
+# inner join 
+# ANIMAL_OUTS as ao
+# on ai.ANIMAL_ID = ao.ANIMAL_ID
+# where ao.DATETIME is not null
+# AND
+# ai.DATETIME
+
+-- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL
+
+SELECT ANIMAL_ID, NAME FROM ANIMAL_OUTS
+EXCEPT
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
