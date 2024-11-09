b17c79e3378e848fc849e5765f421b9c6ce5cb4a
Angela <56811978+SUJINJEONG012@users.noreply.github.com>
2024-11-10 00:04:53 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,20 +1,14 @@
--- 코드를 입력하세요
-# SELECT ai.ANIMAL_ID, ao.NAME
-# from ANIMAL_INS as ai
-# inner join 
-# ANIMAL_OUTS as ao
-# on ai.ANIMAL_ID = ao.ANIMAL_ID
-# where ao.DATETIME is not null
-# AND
-# ai.DATETIME
+# -- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물
+# select ANIMAL_ID, NAME
+# from ANIMAL_OUTS -- 입양보낸동물 테이블
+# where ANIMAL_ID not in (select ANIMAL_ID from ANIMAL_INS)
+# order by ANIMAL_ID
+# -- 동물보호소에 들어온 동물(ANIMAL_INS)
 
--- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL
--- 차집합
-# SELECT ANIMAL_ID, NAME FROM ANIMAL_OUTS
-# EXCEPT
-# SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
 
-SELECT ANIMAL_ID, NAME
-    FROM ANIMAL_OUTS 
-    WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ANIMAL_INS)
-    ORDER BY ANIMAL_ID
+select ao.ANIMAL_ID, ao.name
+from ANIMAL_OUTS ao
+left join ANIMAL_INS ai on
+ao.ANIMAL_ID = ai.ANIMAL_ID
+where ai.ANIMAL_ID is null
+order by ao.ANIMAL_ID
